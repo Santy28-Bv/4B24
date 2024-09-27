2d3d7f27641d9521e3539d2a34ad12bdd6cbd4d1
Santiago <167657885+Santy28-Bv@users.noreply.github.com>
2024-09-27 17:19:41 -0600

    ya corregidp

---
+++ DOM/a/todoapp/app.js
@@ -0,0 +1,25 @@
+const inputTarea =document.getElementById('tareatxt');
+    const btnAdd = document.getElementById('addtarea');
+    const divTareas = document.getElementById('tareas');
+
+    const addTarea = () =>{
+        const tarea = inputTarea.value;
+   
+        divTareas.appendChild(createTarea(tarea));
+        divTareas.appendChild(p);
+        inputTarea.value = '';
+    
+    }
+
+    const createTarea = (tarea) => {
+        const div = document.createElement('div');
+        const p = document.createElement('p');
+        const button = document.createElement('button');
+        p.innerText = tarea;
+        button.innerText = 'Eliminar';
+        div.appendChild(p);
+        div.appendChild(button);
+        return div;
+    }
+    
+    btnAdd.addEventListener('click', addTarea);
